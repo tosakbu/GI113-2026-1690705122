28172503ac3d13145844b3238971656736df786b
tosakbu <tosak.rojc@bumail.net>
2026-08-25 10:41:08 +0700

    Add leb02

---
+++ Leb02/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Student ID : 1690705122
+ * Name       : ต่อศักดิ์ โรจนกรินทร์
+ * Section    : 129c
+ * No.        : na
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Leb02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
