68f4215babebf89c5404b7fb7a88ad4dcd923a75
tosakbu <tosak.rojc@bumail.net>
2026-08-25 11:03:17 +0700

    Add leb02 Part A

--- Leb02/Program.cs
+++ Leb02/Program.cs
@@ -5,13 +5,44 @@
  * No.        : na
  * Course     : GI113 Computer Programming (GI)
  */
+using static System.Net.Mime.MediaTypeNames;
+
 namespace Leb02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            Console.WriteLine($"AttackPower: {attackPower}");
+            Console.WriteLine($"Crit Multiplier: {critMultiplier}");
+            Console.WriteLine($"Is Boss: {isBoss}");
+            Console.WriteLine();
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP Percent: {hpPercent}%");
+            Console.WriteLine();
+            Console.WriteLine("Kirin takes 60 damage!");
+            currentHp = currentHp - 60;
+            Console.WriteLine();
+            Console.WriteLine("===== BOSS STATUS: AFTER DAMAGE =====");
+            hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            Console.WriteLine($"HP Percent: {hpPercent}%");
+            Console.WriteLine();
+
         }
     }
 }
